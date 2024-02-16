72919bd8e2099d253e50181b160ea27ffdd3d5e5
chazmeister <68334216+chazzawarner@users.noreply.github.com>
2024-02-16 10:42:13 +0000

    Pending changes exported from your codespace

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,34 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        if !scores.contains_key(&team_1_name) {
+            let score = Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            };
+            scores.insert(team_1_name, score);
+        } else {
+            let new_scores = Team {
+                goals_scored: team_1_score + scores.get(&team_1_name).unwrap().goals_scored,
+                goals_conceded: team_2_score + scores.get(&team_1_name).unwrap().goals_conceded,
+            };
+            scores.insert(team_1_name, new_scores);
+        }
+
+        if !scores.contains_key(&team_2_name) {
+            let score = Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            };
+            scores.insert(team_2_name, score);
+        } else {
+            let new_scores = Team {
+                goals_scored: team_2_score + scores.get(&team_2_name).unwrap().goals_scored,
+                goals_conceded: team_1_score + scores.get(&team_2_name).unwrap().goals_conceded,
+            };
+            scores.insert(team_2_name, new_scores);
+        }
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -29,15 +27,31 @@ pub enum Command {
 }
 
 mod my_module {
+    use std::{fs::copy, vec};
+
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
-        }
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                /* Command::Append(times) => {
+                    let repeated_bar = "bar".repeat(*times);
+                    let mut to_push = string.clone();
+                    to_push += &repeated_bar.to_string();
+                    output.push((*to_push).to_string());
+                }, */
+                Command::Append(times) => {
+                    let to_push = format!("{}{}", string, "bar".repeat(*times));
+                    output.push(to_push);
+                },
+            }
+        }   
         output
     }
 }
@@ -45,7 +59,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
